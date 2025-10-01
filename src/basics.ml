45d964100093b22d5987b5f052d263c1acf3b3ba
Xwiggi1 <xwiggi1@lsu.edu>
2025-09-30 22:52:13 -0500

    Update basics.ml

--- src/basics.ml
+++ src/basics.ml
@@ -1,59 +1,70 @@
-let abs x =
-  if x >= 0 then x
-  else (-x)
+let abs x = if x >= 0 then x else (-x)
 
-(* ================= *)
+(***********************************)
 (* Part 1: Non-Recursive Functions *)
-(* ================= *)
+(***********************************)
 
-let rev_tup (a, b) = (b, a)
+let rev_tup (tup : 'a * 'b) =
+  let (a, b) = tup in
+  (b, a)
 
-let rev_triple (a, b, c) = (c, b, a)
+let rev_triple (tup : 'a * 'b * 'c) =
+  let (a, b, c) = tup in
+  (c, b, a)
 
-let is_odd x = x mod 2 <> 0
+let is_odd x =
+  x mod 2 <> 0
 
-let is_older (y1, m1, d1) (y2, m2, d2) =
+let is_older (date1: int * int * int) (date2: int * int * int) =
+  let (y1, m1, d1) = date1 in
+  let (y2, m2, d2) = date2 in
   if y1 < y2 then true
   else if y1 > y2 then false
   else if m1 < m2 then true
   else if m1 > m2 then false
   else d1 < d2
 
-let to_us_format (y, m, d) = (m, d, y)
+let to_us_format (date1: int * int * int) =
+  let (y, m, d) = date1 in
+  (m, d, y)
 
-(* ================= *)
+(*******************************)
 (* Part 2: Recursive Functions *)
-(* ================= *)
+(*******************************)
 
 let rec pow x p =
   if p = 0 then 1
   else x * pow x (p - 1)
 
-let rec fac x =
-  if x = 1 then 1
-  else x * fac (x - 1)
+let rec fac n =
+  if n = 1 then 1
+  else n * fac (n - 1)
 
-(* ================= *)
+(*****************)
 (* Part 3: Lists *)
-(* ================= *)
+(*****************)
 
-let rec get_nth idx lst =
+let rec get_nth ((idx:int), (lst: 'a list)) =
   match lst with
   | [] -> failwith "Index out of bounds"
-  | h :: t -> if idx = 0 then h else get_nth (idx - 1) t
+  | h :: t -> if idx = 0 then h else get_nth (idx - 1, t)
 
 let larger lst1 lst2 =
-  let len1 = List.length lst1 in
-  let len2 = List.length lst2 in
+  let rec length lst =
+    match lst with
+    | [] -> 0
+    | _ :: t -> 1 + length t
+  in
+  let len1 = length lst1 in
+  let len2 = length lst2 in
   if len1 > len2 then lst1
   else if len2 > len1 then lst2
   else []
 
 let sum lst1 lst2 =
-  let rec helper l =
-    match l with
+  let rec sum_list lst =
+    match lst with
     | [] -> 0
-    | h :: t -> h + helper t
+    | h :: t -> h + sum_list t
   in
-  (helper lst1) + (helper lst2)
-
+  sum_list lst1 + sum_list lst2
