6d98553d5f88cf0b4a33dadd360ed0847d34be1d
Xwiggi1 <xwiggi1@lsu.edu>
2025-09-30 22:27:51 -0500

    Update basics.ml

--- src/basics.ml
+++ src/basics.ml
@@ -2,32 +2,57 @@ let abs x =
   if x >= 0 then x
   else (-x)
 
-(***********************************)
+(* ================= *)
 (* Part 1: Non-Recursive Functions *)
-(***********************************)
+(* ================= *)
 
-let rev_tup (tup : 'a * 'b) = failwith "unimplemented"
-let rev_triple (tup : 'a * 'b * 'c) = failwith "unimplemented"
+let rev_tup (a, b) = (b, a)
 
-let is_odd x = failwith "unimplemented"
+let rev_triple (a, b, c) = (c, b, a)
 
-let is_older (date1: int * int * int) (date2: int * int * int) = failwith "unimplemented"
+let is_odd x = x mod 2 <> 0
 
-let to_us_format (date1: int * int * int) = failwith "unimplemented"
-(*******************************)
+let is_older (y1, m1, d1) (y2, m2, d2) =
+  if y1 < y2 then true
+  else if y1 > y2 then false
+  else if m1 < m2 then true
+  else if m1 > m2 then false
+  else d1 < d2
+
+let to_us_format (y, m, d) = (m, d, y)
+
+(* ================= *)
 (* Part 2: Recursive Functions *)
-(*******************************)
+(* ================= *)
 
-let rec pow x p = failwith "unimplemented"
+let rec pow x p =
+  if p = 0 then 1
+  else x * pow x (p - 1)
 
-let rec fac n = failwith "unimplemented"
+let rec fac x =
+  if x = 1 then 1
+  else x * fac (x - 1)
 
-(*****************)
+(* ================= *)
 (* Part 3: Lists *)
-(*****************)
+(* ================= *)
 
-let rec get_nth ((idx:int), (lst: 'a list)) = failwith "unimplemented"
+let rec get_nth idx lst =
+  match lst with
+  | [] -> failwith "Index out of bounds"
+  | h :: t -> if idx = 0 then h else get_nth (idx - 1) t
 
-let larger lst1 lst2 = failwith "unimplemented"
+let larger lst1 lst2 =
+  let len1 = List.length lst1 in
+  let len2 = List.length lst2 in
+  if len1 > len2 then lst1
+  else if len2 > len1 then lst2
+  else []
 
-let sum lst1 lst2 = failwith "unimplemented"
+let sum lst1 lst2 =
+  let rec helper l =
+    match l with
+    | [] -> 0
+    | h :: t -> h + helper t
+  in
+  (helper lst1) + (helper lst2)
